f08afae30092ede7d18aa565de61db838a90c3b2
PranjalAshara <pranjal.ashara8583@gmail.com>
2026-06-15 15:19:38 +0530

    MODULARIZATION COMMIT

---
+++ rock_paper_scissor/rps_modularization.py
@@ -0,0 +1,76 @@
+"""
+In order to make the code more readable and modular
+we
+1) Divide the program into several function 
+2) Avoid DRY(Don't Repeat Yourself)
+"""
+
+import random
+"""
+ask the user to make a choice
+if the choice is invalid
+print error
+let the computer make choice
+print choices(emojis)
+determine winner
+ask the usere if they want to continue
+if not
+terminate
+"""
+# use dictionary for key value pairs key -> value
+#eg) 'r' -> 'rock(🪨)'
+
+
+emojis = {
+    'r':'🪨',
+    'p':'📃',
+    's':'✂️'
+}
+choices = ('r','p','s')
+
+def get_user_choice():
+    while True:
+        user_choice: str = input("Rock Paper Scissor? (r/p/s): ").lower()
+        if(user_choice not in choices):
+            print("Invalid Input, please enter a valid choice")
+        else:
+            return user_choice
+
+def display_choices(user_choice,computer_choice):
+    print(f'You chose: {user_choice} {emojis[user_choice]}')
+    print(f'Computer chose: {computer_choice} {emojis[computer_choice]}')
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print('Tie!')
+
+    elif(
+        (user_choice =='r' and computer_choice =='s')
+        or (user_choice=='p' and computer_choice=='r')
+        or (user_choice=='s' and computer_choice=='p')):
+        print('You win!')
+
+    else:
+        print("Computer wins!")
+
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(choices)
+
+        display_choices(user_choice,computer_choice)
+
+        determine_winner(user_choice,computer_choice)
+
+        play = input('Wanna play Rock, Paper, Scissors?(y/n): ').lower()
+        if play not in ('y','n'):
+            print("Please enter 'y' or 'n' ")
+        if(play == 'y'):
+            continue
+        if(play == 'n'):
+            print("Maybe we'll play next time")
+            break
+        
+play_game()
+
